d7ea7d9134ca0fc8dd009ce815f6a6d168a94f5a
nishizaw-a <n00284k@gmail.com>
2022-11-24 16:46:33 +0900

    Upload files.

---
+++ QuizList/src/powerOfTwo/PowerOfTwo.java
@@ -0,0 +1,9 @@
+package powerOfTwo;
+
+public class PowerOfTwo {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ QuizList/src/powerOfTwo/PowerOfTwoAnswer.java
@@ -0,0 +1,9 @@
+package powerOfTwo;
+
+public class PowerOfTwoAnswer {
+
+	public static void main(String[] args) {
+
+	}
+
+}
