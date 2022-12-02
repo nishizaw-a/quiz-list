9bf013ecf6c452e0fd6da5934f1cba89e27d2fce
nishizaw-a <n00284k@gmail.com>
2022-12-02 12:39:40 +0900

    Upload files.

---
+++ QuizList/src/matrixMultiplication/MatrixMultiplication.java
@@ -0,0 +1,9 @@
+package matrixMultiplication;
+
+public class MatrixMultiplication {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ QuizList/src/matrixMultiplication/MatrixMultiplicationAnswer.java
@@ -0,0 +1,9 @@
+package matrixMultiplication;
+
+public class MatrixMultiplicationAnswer {
+
+	public static void main(String[] args) {
+
+	}
+
+}
