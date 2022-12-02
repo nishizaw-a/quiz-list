088fbe574d3bd277cb85a45e1a4447e3eda1baea
nishizaw-a <n00284k@gmail.com>
2022-12-02 12:30:29 +0900

    Upload files.

---
+++ QuizList/src/fizzBuzzProblem/FizzBuzzProblem.java
@@ -0,0 +1,9 @@
+package fizzBuzzProblem;
+
+public class FizzBuzzProblem {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ QuizList/src/fizzBuzzProblem/FizzBuzzProblemAnswer.java
@@ -0,0 +1,9 @@
+package fizzBuzzProblem;
+
+public class FizzBuzzProblemAnswer {
+
+	public static void main(String[] args) {
+
+	}
+
+}
