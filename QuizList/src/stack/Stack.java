85e4581466ffd17380f768ef1ac0e7c5e4e0026d
nishizaw-a <n00284k@gmail.com>
2022-12-08 17:36:35 +0900

    Complete.

--- QuizList/src/stack/Stack.java
+++ QuizList/src/stack/Stack.java
@@ -1,9 +1,63 @@
 package stack;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Stack {
 
+	/*
+	 *データ構造の一つにスタックがあります。スタックとは以下の条件を満たすデータ構造です。
+	 *
+	 *追加された要素は最後尾に追加される (push)
+	 *取り出される要素は最後が取り出される(pop)
+	 *
+	 *このデータ構造を満たすようpop、pushメソッドを実装してStackを完成させ実行結果と同じ実行結果になるように実装してください。
+	 *
+	 *-----実行結果-----
+	 *==> 3, 1, 4
+	 *==> 3, 1
+	 *==> 3, 1, 2
+	 *==> 3, 1
+	 *==> 3
+	 *==>
+	 */
+
+	private List<Integer> stack;
+
 	public static void main(String[] args) {
+		Stack stack = new Stack();
+		stack.push(3);
+		stack.push(1);
+		stack.push(4);
+		stack.print();
+		stack.pop();
+		stack.print();
+		stack.push(2);
+		stack.print();
+		stack.pop();
+		stack.print();
+		stack.pop();
+		stack.print();
+		stack.pop();
+		stack.print();
+	}
+
+	public Stack() {
+		this.stack = new ArrayList<Integer>();
+	}
+
+	public Integer pop() {
 
 	}
+
+	public void push(Integer value) {
+
+	}
+
+	public void print() {
+		//正規表現 [, ] は\\でエスケープ処理 |で複数指定
+		System.out.println("==> " + this.stack.toString().replaceAll("\\[|\\]", ""));
+	}
+
 
 }
--- QuizList/src/stack/StackAnswer.java
+++ QuizList/src/stack/StackAnswer.java
@@ -1,9 +1,64 @@
 package stack;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StackAnswer {
 
-	public static void main(String[] args) {
+	/*
+	 *データ構造の一つにスタックがあります。スタックとは以下の条件を満たすデータ構造です。
+	 *
+	 *追加された要素は最後尾に追加される (push)
+	 *取り出される要素は最後が取り出される(pop)
+	 *
+	 *このデータ構造を満たすようpop、pushメソッドを実装してStackを完成させ実行結果と同じ実行結果になるように実装してください。
+	 *
+	 *-----実行結果-----
+	 *==> 3, 1, 4
+	 *==> 3, 1
+	 *==> 3, 1, 2
+	 *==> 3, 1
+	 *==> 3
+	 *==>
+	 */
 
+	private List<Integer> stack;
+
+	public static void main(String[] args) {
+		StackAnswer stack = new StackAnswer();
+		stack.push(3);
+		stack.push(1);
+		stack.push(4);
+		stack.print();
+		stack.pop();
+		stack.print();
+		stack.push(2);
+		stack.print();
+		stack.pop();
+		stack.print();
+		stack.pop();
+		stack.print();
+		stack.pop();
+		stack.print();
+	}
+
+	public StackAnswer() {
+		this.stack = new ArrayList<Integer>();
+	}
+
+	public Integer pop() {
+		Integer value = this.stack.get(this.stack.size() - 1);//最後の要素を取り出す
+		this.stack.remove(this.stack.lastIndexOf(value));//取り出した要素をリストから削除
+		return value;
+	}
+
+	public void push(Integer value) {
+		this.stack.add(value);//後ろに追加する
+	}
+
+	public void print() {
+		//正規表現 [, ] は\\でエスケープ処理 |で複数指定
+		System.out.println("==> " + this.stack.toString().replaceAll("\\[|\\]", ""));
 	}
 
 }
