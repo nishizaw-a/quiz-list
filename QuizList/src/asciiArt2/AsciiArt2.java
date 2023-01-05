734aead24373e95962a8ccab07e40335841dd129
nishizaw-a <n00284k@gmail.com>
2023-01-05 09:45:03 +0900

    Upload files.

---
+++ QuizList/src/asciiArt2/AsciiArt2.java
@@ -0,0 +1,9 @@
+package asciiArt2;
+
+public class AsciiArt2 {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ QuizList/src/asciiArt2/AsciiArt2Answer.java
@@ -0,0 +1,9 @@
+package asciiArt2;
+
+public class AsciiArt2Answer {
+
+	public static void main(String[] args) {
+
+	}
+
+}
