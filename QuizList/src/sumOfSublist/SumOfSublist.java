8ca56c7e92ff7276e3f6457d4072f6f24b5821a9
nishizaw-a <n00284k@gmail.com>
2023-02-01 14:30:29 +0900

    Upload files

---
+++ QuizList/src/sumOfSublist/SumOfSublist.java
@@ -0,0 +1,9 @@
+package sumOfSublist;
+
+public class SumOfSublist {
+
+  public static void main(String[] args) {
+
+  }
+
+}
---
+++ QuizList/src/sumOfSublist/SumOfSublistAnswer.java
@@ -0,0 +1,9 @@
+package sumOfSublist;
+
+public class SumOfSublistAnswer {
+
+  public static void main(String[] args) {
+
+  }
+
+}
